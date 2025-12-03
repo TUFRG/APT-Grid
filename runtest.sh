b96677b244eaf15ea34c3495ab8a0e44c198362a
jdefoe2 <jdefoe2@users.noreply.github.com>
2025-12-03 11:15:11 -0500

    Updates for robustness
    Remove VTK folder at beginning
    Run foamToVTK at end in case there are no non-ortho faces to avoid quitting on error.

--- runtest.sh
+++ runtest.sh
@@ -3,11 +3,12 @@
 set -e
 
 clear
+rm -rf VTK
 cp ~/Documents/Work/UWindsor/Research/OpenFOAMturbomachinery/structuredMeshWithProjection/TUFRG/surfaceGenerator/ECL5_original/periodic/passageParameters system/
 cp ~/Documents/Work/UWindsor/Research/OpenFOAMturbomachinery/structuredMeshWithProjection/TUFRG/surfaceGenerator/ECL5_original/periodic../constant/geometry1/*.stl constant/geometry/
 ./geomUpdate.sh
 blockMesh
 checkMesh
-foamToVTK -faceSet nonOrthoFaces
 createPatch
 rm -rf 1
+foamToVTK -faceSet nonOrthoFaces
